4d95d5b755186ad1d365c80963ab30bf1d518f58
Applebeef <58487936+Applebeef@users.noreply.github.com>
2022-04-15 18:47:53 +0300

    started pandas 1.1

--- pandas_seq.py
+++ pandas_seq.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
+
+def three_x_plus_1(s: pd.Series) -> pd.Series:
+    # can we use numpy in this part? if so, we can use np.where()
+    return s.apply(lambda x: x / 2 if x % 2 == 0 else 3 * x + 1)
+
+
 if __name__ == "__main__":
-    x = 3  # delete this line, this is meant to stop pycharm from complaining
+    s = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    print(s)
+    print(three_x_plus_1(s))
