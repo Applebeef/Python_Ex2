6493224027fa763a769d240e6bc2118dadcf0dd0
Applebeef <58487936+Applebeef@users.noreply.github.com>
2022-04-17 18:59:33 +0300

    started pandas 1.1

--- pandas_seq.py
+++ pandas_seq.py
@@ -1,12 +1,31 @@
 import pandas as pd
+import numpy as np
 
 
-def three_x_plus_1(s: pd.Series) -> pd.Series:
-    # can we use numpy in this part? if so, we can use np.where()
-    return s.apply(lambda x: x / 2 if x % 2 == 0 else 3 * x + 1)
+def three_x_plus_1(s):
+    return pd.Series(np.where(s % 2 == 0, s / 2, 3 * s + 1))
+
+
+def no_nans_idx(s):
+    return pd.Series(index=s.values, data=np.where(np.isnan(s), False, True))
+
+
+def partial_eq(s1, s2):
+    indexes = s1.index.intersection(s2.index)
+    return pd.Series(index=indexes, data=np.where(s1[indexes] == s2[indexes], True, False))
+
+
+def get_n_largest(df, n=0, how='col'):
+    # TODO implement 2.7
+    pass
+
+
+def upper(df):
+    return df.apply(lambda x: x.apply(lambda y: y.upper() if isinstance(y, str) else y))
 
 
 if __name__ == "__main__":
-    s = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    print(s)
-    print(three_x_plus_1(s))
+    df = pd.DataFrame(np.random.randint(0, 100, size=(5, 4)), columns=list('ABCD'))
+    df.iloc[2, 2] = 'string'
+    print(df)
+    print(upper(df))
