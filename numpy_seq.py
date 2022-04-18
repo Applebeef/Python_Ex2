4671d4a653613f8ae8d9e9765dcc19a3be6b290e
TalAharon23 <talaha@mta.ac.il>
2022-04-18 17:20:47 +0300

    1.6 1.9

--- numpy_seq.py
+++ numpy_seq.py
@@ -46,5 +46,82 @@ def segment(im, thresh=128):
     return np.where(im < thresh, 0, 255)
 
 
+# Ex 1.6
+def getMinorFromMat(A, i, j):  # Getting the new sub matrix without col j.
+    return [row[: j] + row[j + 1:] for row in (A[: i] + A[i + 1:])]
+
+
+def det(A):  # Recursive func - Calculates the determinant of square matrix A.
+    if len(A) == 2:
+        value = (A[0][0] * A[1][1]) - (A[1][0] * A[0][1])
+        return value
+
+    res = 0
+    for col in range(len(A)):
+        sign = (-1) ** col
+        subDet = det(getMinorFromMat(A, 0, col))
+        res += (sign * A[0][col]) * subDet
+
+    return res
+
+
+# Ex 1.8
+def linearville(robber, policeman):
+    # robber = numpy.ndarray of shape (h,n,d) of integers.
+    # policeman = numpy.ndarray of shape (h,n) of integers.
+    print("Sgssg")
+
+
+# Ex 1.9
+def isAllElementsDistinct(matrix):
+    size = len(matrix)
+
+    elementsSet = set()
+    for i in range(0, size):
+        for j in range(0, size):
+            elementsSet.add(matrix[i][j])
+
+    return len(matrix)*len(matrix) == len(elementsSet)
+
+
+def is_magic(matrix):
+    sumDiagonal1 = np.trace(matrix)
+
+    secondDiagonal = np.diagonal(np.fliplr(matrix))
+    sumDiagonal2 = sum(secondDiagonal)
+
+    if sumDiagonal1 != sumDiagonal2:
+        return False
+
+    rowsSum = np.sum(matrix, axis=0)
+    colsSum = np.sum(matrix, axis=1)
+
+    # Check if all values in an array are equal to its first element
+    isValuesInRowsAreSame = np.all(rowsSum == rowsSum[0])
+    isValuesInColsAreSame = np.all(colsSum == colsSum[0])
+
+    if isValuesInRowsAreSame and isValuesInColsAreSame and (
+            rowsSum[0] == colsSum[0] == sumDiagonal1) and isAllElementsDistinct(matrix):
+        return True
+
+    return False
+
+
 if __name__ == "__main__":
     x = 3  # delete this line
+
+    # mat = [[1, 0, 2, -1],
+    #        [3, 0, 0, 5],
+    #        [2, 1, 4, -3],
+    #        [1, 0, 5, 0]]
+
+    mat = [[4, 9, 2],
+           [3, 5, 7],
+           [8, 1, 6]]
+
+    if is_magic(mat):
+        print("YESS")
+    else:
+        print("NOOO")
+
+    # print('Determinant of the matrix is :', det(mat))
